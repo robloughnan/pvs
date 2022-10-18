a291f353d053b53a78c67e18f0c13b4d212be4a0
Rob - Chuns machine <rloughna@ucsd.edu>
2022-10-17 18:42:44 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# Introduction
+This lightweight tool can be used to generate a PolyVoxel Score across a set of MNI registered T2-Weighted scans capturing the iron deposition seen in the architypal "Hemochromatosis Brain". This pattern of brain iron desposition may relevant for Parkinson's Disease risk. Two papers discussing details can be found here:
+
+[Loughnan, R., et al. Association of Genetic Variant Linked to Hemochromatosis With Brain Magnetic Resonance Imaging Measures of Iron and Movement Disorders. JAMA Neurology, 2015.](https://jamanetwork.com/journals/jamaneurology/fullarticle/2794928) 
+
+[BIORXIV](https://jamanetwork.com/journals/jamaneurology/fullarticle/2794928) 
+
+# Getting Started
+
+In order to download `pvs` you should clone this repository via the following command:
+
+```
+git clone https://github.com/robloughnan/pvs.git
+cd pvs
+```
+
+To install dependancies (`nibabel`, `matplotlib` and `numpy`) you can use `conda` package manager availible [here.](https://store.continuum.io/cshop/anaconda/)
+
+Once conda installed you can create a new environement with pvs dependencies.
+
+```
+conda env create --file environment.yml
+source activate pvs
+```
+
+# How to Run
+
+`pvs` has two simple functionalities:
+
+1.  Provide plots of input scans overlaid with PVS weights to verify scans are aligned correctly (`--check_reg` option)
+2. Generate PolyVertex Scores using weights generated from [this analysis](ref) on set of images (`--nii_files` option)
+
+## Check Registration
+
+`pvs` assumes that the T2-Weighted scans you are trying to generate PolyVoxel Scores for are already registered to MNI space. To check this you can run the following:
+
+```
+python pvs --check_reg INPUT_SCAN.nii --out out_reg_check.pdf
+```
+
+Replacing `INPUT_SCAN.nii` with your input T2-Weighted scan. This should generate a file that looks like this:
+
+![alt text](./mni152_reg_check.pdf "Title")
+
+This should enable you to verify that the weights are aligned with your image (i.e. peaks in the basal ganglia and cerebellum)
+
+
+## Generate PolyVoxel Score
+
+Using the option `--nii_files` you can pass the filepath to a text file containing a list of NIFTI files which you would like to generate PolyVoxel Scores for. An example of the contents of this file (named `nifti_files.txt`):
+```
+/path/to/scan_1.nii
+/path/to/scan_2.nii
+/path/to/scan_3.nii
+/path/to/scan_4.nii
+...
+```
+
+You can then generate PVS's for each of these scans using:
+```
+python pvs --nii_files nifti_files.txt --out PVS_out.tsv
+```
+```
+Image                   PVS
+/path/to/scan_1.nii     1.52
+/path/to/scan_2.nii     -1.32
+/path/to/scan_3.nii     3.6
+/path/to/scan_4.nii     -0.2
+...
+```
+
+## Citation
+
+If you use this software please cite:
+
+[Loughnan, R., et al. Association of Genetic Variant Linked to Hemochromatosis With Brain Magnetic Resonance Imaging Measures of Iron and Movement Disorders. JAMA Neurology, 2015.](https://jamanetwork.com/journals/jamaneurology/fullarticle/2794928) 
+
+and 
+
+[BIORXIV](https://jamanetwork.com/journals/jamaneurology/fullarticle/2794928) 
---
+++ pvs.py
@@ -0,0 +1,111 @@
+import nibabel as nib
+from nibabel import processing
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import argparse
+import pandas as pd
+
+def read_nii(nii_file, stats):
+    ## NEED TO DOCUMENT HERE
+    # Read in user defined file 
+    print(f'Reading in {nii_file}')
+    nii_img = nib.load(nii_file)
+
+    # Check images have the same dimensions
+    if len(nii_img.shape) != 3:
+        return None
+
+    if nii_img.shape != stats.shape:
+        print(f'Resampling {nii_file} (shape: {nii_img.shape}) to match assocation stats (shape: {stats.shape})')
+        nii_img = processing.resample_from_to(nii_img, stats)
+    
+    return nii_img
+
+def read_and_mask(nifti_file, mask):
+    ## NEED TO DOCUMENT HERE
+    pass
+    
+
+def check_reg(nii_file, out):
+    ### Function reads in nii file and plots out to file overlaying PVS to check alignment
+    # nii_file: (str) path to nii_file to generate overlay on top of
+    # out: (str) path to output for generated file, if None then will output to same directory as nii_file
+
+    if nii_file.endswith('.nii') and  nii_file.endswith('.nii.gz'):
+        raise ValueError('Please pass NIFTI filepath')
+
+    # Read in association statistics
+    tstats = nib.load('./t_stats.nii')
+    tstats_data = tstats.get_fdata()
+    ind = abs(tstats_data[:, :, :].T)<3.5
+    tstats_data.T[ind] = np.nan
+
+    # Extract image
+    nii_img = read_nii(nii_file, tstats)
+    if nii_img is None:
+        raise ValueError(f'{nii_file} is not 3d image')
+
+    nii_img_data = nii_img.get_fdata()
+
+    # Generate plot
+    print('Generating plot and overlay')
+    alpha = 0.8
+    cm_stats = 'jet'
+
+    axial_slcs = [85, 80, 70, 60, 45, 40]
+    n_cols = int(np.ceil(len(axial_slcs)/2))
+    fig, axes = plt.subplots(2, n_cols, figsize=(16, 10))
+    for ind, axial_slc in enumerate(axial_slcs):
+        i, j = np.unravel_index(ind, (2, n_cols))
+        axes[i, j].imshow(nii_img_data[:, :, axial_slc].T, cmap="gray", origin="lower")
+        axes[i, j].imshow(tstats_data[:, :, axial_slc].T, cmap=cm_stats, origin="lower", alpha=alpha)
+        axes[i, j].set_xticks([])
+        axes[i, j].set_yticks([])
+
+    # If out is not defined then use nii_file path
+    if out is None:
+        out_parts = os.path.split(nii_file)
+        out = os.path.join(out_parts[0], out_parts[1].split('.')[0] + '_reg_check.pdf')
+        print(f'No user defined output, saving to {out}')
+    
+    plt.tight_layout()
+    plt.savefig(out)
+
+def compute_pvs(nii_files, out):
+    ## NEED TO DOCUMENT HERE
+
+    # Read in text files
+    nii_files = pd.read_csv(nii_files, header=None).iloc[:, 0]
+
+    # Read in tstats in case scans need reslicing
+    tstats = nib.load('./t_stats.nii')
+    # Generate mask of most significant voxels
+
+    # Read in nifti files (applying mask)
+    masked_images = nii_files.apply(lambda x: read_and_mask(x, mask))
+
+    # Generate PolyVoxel Score
+    pvs = masked_images * weights
+    pvs = pd.DataFrame({'Scan': nii_files.tolist(), 'PVS': pvs})
+
+    # Save out results
+    if out is None:
+        out = './pvs.tsv'
+    pvs.to_csv(out, sep='\t')
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Command line tool generates a PolyVoxel Score (PVS) of the "Hemochromatosis Brain" using T2-Weighted NIFTI scans')
+    parser.add_argument('--check_reg', help='Pass .nii file to check if it is registered with weights and MNI space before computing PVS', type=str, default=None)
+    parser.add_argument('--nii_files', help='Pass path to text file containing filepaths to .nii files registered to MNI space', type=str, default=None)
+    parser.add_argument('--out', help='Path to output', type=str, default=None)
+
+
+    opt = parser.parse_args()
+
+    if not opt.check_reg is None:
+        check_reg(opt.check_reg, opt.out)
+    elif not opt.nii_files is None:
+        compute_pvs(opt.nii_files, opt.out)
+    
